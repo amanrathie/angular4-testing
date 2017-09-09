98fa4db940d2d5b0ff9249bacf29dc6a6764f809
Aman Rathie <amanrathie@gmail.com>
2017-09-09 20:05:42 -0300

    Making auth0 work

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -7,5 +7,5 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: 'gALKwsjpK_nbC3fuczFkcqz1IGDqo9Oi',
   domain: 'amanrathie.auth0.com',
-  callbackURL: 'https://poker-bankroll.herokuapp.com/home/dashboard'
+  callbackURL: 'https://poker-bankroll.herokuapp.com/callback'
 };
