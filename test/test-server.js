6ca84c7eef94825229bb694afe36d5d6d388c083
Aman Rathie <amanrathie@gmail.com>
2017-09-24 23:40:29 -0300

    Improving tests

--- test/test-server.js
+++ test/test-server.js
@@ -6,12 +6,25 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Games', function() {
-  it('should list ALL games on /game GET', function(done) {
+  it('should list ALL games on /game GET', (done) => {
     chai.request(server)
-      .get('/api/game')
-      .end(function(err, res){
-        res.should.have.status(200);
-        done();
-      });
+        .get('/api/game')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.json;
+          done();
+        });
+  });
+
+  it('should add a SINGLE game on /game POST', function(done) {
+    chai.request(server)
+        .post('/api/game')
+        .send({'location':'Brasilia', 'amountSpended':'1000', 'amountEarned':'2000'})
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.should.be.json;
+          res.body.should.have.property('_id');
+          done();
+        })
   });
 });
