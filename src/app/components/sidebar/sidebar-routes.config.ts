d00653b93556193c89afc79ffbebd323aa20935c
Aman Rathie <amanrathie@gmail.com>
2017-09-27 00:55:14 -0300

    Removing game type menu

--- src/app/components/sidebar/sidebar-routes.config.ts
+++ src/app/components/sidebar/sidebar-routes.config.ts
@@ -2,9 +2,9 @@ import {  RouteInfo } from './sidebar.metadata';
 
 export const ROUTES: RouteInfo[] = [
     { path: 'dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: 'game', title: 'Games',  icon: 'library_books', class: '' },
-    { path: 'game-type', title: 'Game Types',  icon: 'content_paste', class: '' },
-    /*{ path: 'user-profile', title: 'User Profile',  icon:'person', class: '' },
+    { path: 'game', title: 'Games',  icon: 'library_books', class: '' }
+    /*{ path: 'game-type', title: 'Game Types',  icon: 'content_paste', class: '' },
+    { path: 'user-profile', title: 'User Profile',  icon:'person', class: '' },
     { path: 'table-list', title: 'Table List',  icon:'content_paste', class: '' },
     { path: 'typography', title: 'Typography',  icon:'library_books', class: '' },
     { path: 'icons', title: 'Icons',  icon:'bubble_chart', class: '' },
